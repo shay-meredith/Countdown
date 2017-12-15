87815bc3e521fd258d1d9aec32c24fbac35ce631
Shay Meredith <shay.e.meredith@gmail.com>
2017-12-15 09:38:28 -0500

    Create clock object in JS

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //TODO: Try object oriented JS
+
+d = new Date();
+console.log(d);
+
+let clock = {
+    hour: d.getHours(),
+    minute: d.getMinutes(),
+    second: d.getSeconds(),
+    month: d.getMonth(),
+    day: d.getDay(),
+    year: d.getFullYear()
+}
+
+console.log(clock.hour);
