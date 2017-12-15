734c042cd885eba67781a22b5313aa2b22296724
Shay Meredith <shay.e.meredith@gmail.com>
2017-12-15 10:29:48 -0500

    Add time display
    Not well executed code

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 //TODO: Try object oriented JS
 
+function addTime() {
+    document.getElementsByTagName("h1")[0].style.display = "inline";
+    document.getElementsByTagName("h1")[1].style.display = "inline";
+    document.getElementById("time-insert").innerHTML = `${clock.hour}:${clock.minute}`;
+}
 d = new Date();
 console.log(d);
 
